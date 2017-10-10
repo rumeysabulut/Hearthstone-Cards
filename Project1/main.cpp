f6f094c04ef008545cb9d2408bc01612862c4100
Rumeysa Bulut <rumeysabulut@Rumeysa-MacBook-Air.local>
2017-10-10 10:22:48 +0300

    Initial Commit

---
+++ Project1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project1
+//
+//  Created by Rumeysa Bulut on 10.10.2017.
+//  Copyright © 2017 Rumeysa Bulut. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
